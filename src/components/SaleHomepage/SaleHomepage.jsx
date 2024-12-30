cf3bb928df028cf2b1f3194a58038fca86e88115
NguyenHao <Henryporison@outlook.com.vn>
2024-12-30 22:16:21 +0700

    30.1

--- src/components/SaleHomepage/SaleHomepage.jsx
+++ src/components/SaleHomepage/SaleHomepage.jsx
@@ -1,14 +1,24 @@
 import styles from './styles.module.scss';
+import Button from '../Button/Button';
 
 function SaleHomepage() {
-    const { container } = styles;
+    const { container, content } = styles;
 
     return (
         <div className={container}>
             <div>
-                <div>Image1</div>
-                <div>content</div>
-                <div>IMG2 </div>
+                <img src='src\assets\images\banner2.avif' alt='' />
+            </div>
+            <div className={content}>
+                <h2>Sale Of The Year</h2>
+                <p>
+                    To celebrate the Year of the Snake, we’ve created a
+                    one-of-a-kind Emberton III speaker.
+                </p>
+                <Button content={'Read More'} />
+            </div>
+            <div>
+                <img src='src\assets\images\banner4.avif' alt='' />{' '}
             </div>
         </div>
     );
