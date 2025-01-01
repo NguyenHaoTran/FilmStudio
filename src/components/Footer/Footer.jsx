a77d580ac91aabbc55897ff28c08c5afd324b946
NguyenHao <Henryporison@outlook.com.vn>
2025-01-01 12:21:06 +0700

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,31 @@
+import { dataMenu } from '@components/Footer/constants';
 import styles from './styles.module.scss';
 
 function MyFooter() {
-    const { container } = styles;
-    return <div className={container}> My Footer </div>;
+    const { container, boxNav } = styles;
+    return (
+        <div className={container}>
+            <div>
+                <img
+                    src='src\assets\icons\images\LogoMarshall.png'
+                    alt=''
+                    width={160}
+                />
+            </div>
+            <div className={boxNav}>
+                {dataMenu.map((item) => (
+                    <div key={item.id}>{item.content}</div>
+                ))}
+            </div>
+            <div>
+                <p style={{ textAlign: 'center' }}>Guaranteed safe ckeckout</p>
+                <img src='src\assets\icons\iconPayment.png' alt='' />
+            </div>
+            <div>
+                <p>Copyright © 2024. Created by Ng.hao.t</p>
+            </div>
+        </div>
+    );
 }
 
 export default MyFooter;
