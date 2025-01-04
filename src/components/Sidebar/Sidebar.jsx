c9dc079c3387a82033bcab891c23c63070b746f0
ng.hao.t <ng.hao.t@nghaots-iMac-Pro.local>
2025-01-04 22:48:11 +0700

    donesidebar_context

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,15 +2,35 @@ import { useContext } from 'react';
 import styles from './styles.module.scss';
 import { SideBarContext } from '../../contexts/SidebarProvider';
 import classNames from 'classnames';
+// icons
+import { IoMdClose } from 'react-icons/io';
 
 function Sidebar() {
-    const { container, overlay, sideBar } = styles;
-    const { isOpen } = useContext(SideBarContext);
+    const { container, overlay, sideBar, slideSideBar, boxIcon } = styles;
+    const { isOpen, setIsOpen } = useContext(SideBarContext);
+
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div className={container}>
-            <div className={classNames({ [overlay]: isOpen })} />
-            <div className={sideBar}>sideBar</div>
+            <div
+                className={classNames({ [overlay]: isOpen })}
+                onClick={handleToggle}
+            />
+            <div
+                className={classNames(sideBar, {
+                    [slideSideBar]: isOpen
+                })}
+            >
+                {isOpen && (
+                    <div className={boxIcon} onClick={handleToggle}>
+                        <IoMdClose />
+                    </div>
+                )}
+                sideBar
+            </div>
         </div>
     );
 }
