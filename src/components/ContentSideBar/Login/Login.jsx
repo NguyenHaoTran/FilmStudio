f56b2d0daec78b4f2efa9bb249702311a8ada010
ng.hao.t <ng.hao.t@nghaots-iMac-Pro.local>
2025-01-06 11:18:58 +0700

    login

--- src/components/ContentSideBar/Login/Login.jsx
+++ src/components/ContentSideBar/Login/Login.jsx
@@ -1,13 +1,21 @@
 import InputCommon from '../../InputCommon/InputCommon';
 import styles from './styles.module.scss';
+import Button from '../../Button/Button';
 
 function Login() {
-    const { container, title } = styles;
+    const { container, title, boxRememberMe, lostPw } = styles;
 
     return (
         <div className={container}>
             <div className={title}>SIGN IN</div>
-            <InputCommon />
+            <InputCommon label='Email' type='text' isRequired />
+            <InputCommon label='Password' type='password' isRequired />
+            <div className={boxRememberMe}>
+                <input type='checkbox' />
+                <span>Remember me</span>
+            </div>
+            <Button content={'LOGIN'} />
+            <div className={lostPw}>Lost your password?</div>
         </div>
     );
 }
--- src/components/InputCommon/InputCommon.jsx
+++ src/components/InputCommon/InputCommon.jsx
@@ -1,18 +1,37 @@
+import { Balance } from '@mui/icons-material';
 import styles from './styles.module.scss';
+import { useState } from 'react';
+// icons
+import { IoIosEye } from 'react-icons/io';
+import { FaEyeSlash } from 'react-icons/fa';
 
-function InputCommon() {
-    const { container, labelInput, boxInput } = styles;
+function InputCommon({ label, type, isRequired = false }) {
+    const { container, labelInput, boxInput, boxIcon } = styles;
+    const [showPassword, setShowPassword] = useState(false);
+
+    const isPassword = type === 'password';
+    const isShowTextPassWord =
+        type === 'password' && showPassword ? 'text' : type;
+
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
 
     return (
         <div className={container}>
-            <div className={labelInput}>UserName or email</div>
+            <div className={labelInput}>
+                {label} {isRequired && <span>*</span>}
+            </div>
             <div className={boxInput}>
-                <input type='text' />
+                <input type={isShowTextPassWord} />
+                {isPassword && (
+                    <div className={boxIcon} onClick={handleShowPassword}>
+                        {showPassword ? <FaEyeSlash /> : <IoIosEye />}
+                    </div>
+                )}
             </div>
         </div>
     );
 }
 
 export default InputCommon;
-
-//  13_ 12:16
