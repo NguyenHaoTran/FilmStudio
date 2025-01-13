b854a207e1e301fa0b89c8eea8e3e35410827c45
ng.hao.t <ng.hao.t@nghaots-iMac-Pro.local>
2025-01-13 21:25:59 +0700

    formit

--- src/components/ContentSideBar/Login/Login.jsx
+++ src/components/ContentSideBar/Login/Login.jsx
@@ -8,19 +8,64 @@ import * as Yup from 'yup';
 function Login() {
     const { container, title, boxRememberMe, lostPw } = styles;
 
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email('Invalid Email')
+                .required('Email is required'),
+            password: Yup.string()
+                .min(6, 'Password must be at least 6 characters')
+                .required('Password is required')
+        }),
+        //
+        onSubmit: (values) => {
+            console.log(values);
+        }
+        //
+    });
+
+    console.log(formik.errors);
+
     return (
         <div className={container}>
             <div className={title}>SIGN IN</div>
-            <InputCommon label='Email' type='text' isRequired />
-            <InputCommon label='Password' type='password' isRequired />
-            <div className={boxRememberMe}>
-                <input type='checkbox' />
-                <span>Remember me</span>
-            </div>
-            <Button content={'LOGIN'} />
+            <form onSubmit={formik.handleSubmit}>
+                <InputCommon
+                    id='email'
+                    label='Email'
+                    type='text'
+                    isRequired
+                    // onBlur={formik.handleBlur}
+                    // onChange={formik.handleChange}
+                    // value={formik.values.email}
+                    formik={formik}
+                />
+
+                <InputCommon
+                    id='password'
+                    label='Password'
+                    type='password'
+                    isRequired
+                    // onBlur={formik.handleBlur}
+                    // onChange={formik.handleChange}
+                    // value={formik.values.password}
+                    formik={formik}
+                />
+
+                <div className={boxRememberMe}>
+                    <input type='checkbox' />
+                    <span>Remember me</span>
+                </div>
+                <Button content={'LOGIN'} type='submit' />
+            </form>
             <div className={lostPw}>Lost your password?</div>
         </div>
     );
 }
 
 export default Login;
+// 3.19
--- src/components/InputCommon/InputCommon.jsx
+++ src/components/InputCommon/InputCommon.jsx
@@ -5,8 +5,9 @@ import { useState } from 'react';
 import { IoIosEye } from 'react-icons/io';
 import { FaEyeSlash } from 'react-icons/fa';
 
-function InputCommon({ label, type, isRequired = false }) {
-    const { container, labelInput, boxInput, boxIcon } = styles;
+function InputCommon({ label, type, isRequired = false, ...props }) {
+    const { container, labelInput, boxInput, boxIcon, errMsg } = styles;
+    const { formik, id } = props;
     const [showPassword, setShowPassword] = useState(false);
 
     const isPassword = type === 'password';
@@ -17,18 +18,29 @@ function InputCommon({ label, type, isRequired = false }) {
         setShowPassword(!showPassword);
     };
 
+    const isErr = formik.touched[id] && formik.errors[id];
+    const messageErr = formik.errors[id];
+
     return (
         <div className={container}>
             <div className={labelInput}>
                 {label} {isRequired && <span>*</span>}
             </div>
             <div className={boxInput}>
-                <input type={isShowTextPassWord} />
+                <input
+                    type={isShowTextPassWord}
+                    {...props}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values[id]}
+                />
                 {isPassword && (
                     <div className={boxIcon} onClick={handleShowPassword}>
                         {showPassword ? <FaEyeSlash /> : <IoIosEye />}
                     </div>
                 )}
+
+                {isErr && <div className={errMsg}>{messageErr}</div>}
             </div>
         </div>
     );
