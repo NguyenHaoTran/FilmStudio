7acead5439b94abcc599a485657dd1da9ec65f05
ng.hao.t <ng.hao.t@nghaots-iMac-Pro.local>
2025-01-17 19:18:44 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,32 @@ import routers from '@/routers/routers';
 import { Suspense } from 'react';
 import { SidebarProvider } from './contexts/SideBarProvider';
 import Sidebar from './components/Sidebar/Sidebar';
+import { ToastProvider } from '@/contexts/ToastProvider';
 
 function App() {
     return (
-        <SidebarProvider>
-            <Sidebar />
-            <BrowserRouter>
-                <Suspense fallback={<div className='loading'>Loading...</div>}>
-                    <Routes>
-                        {routers.map((item, index) => {
-                            return (
-                                <Route
-                                    path={item.path}
-                                    element={<item.component />}
-                                    key={index}
-                                />
-                            );
-                        })}
-                    </Routes>
-                </Suspense>
-            </BrowserRouter>
-        </SidebarProvider>
+        <ToastProvider>
+            <SidebarProvider>
+                <Sidebar />
+                <BrowserRouter>
+                    <Suspense
+                        fallback={<div className='loading'>Loading...</div>}
+                    >
+                        <Routes>
+                            {routers.map((item, index) => {
+                                return (
+                                    <Route
+                                        path={item.path}
+                                        element={<item.component />}
+                                        key={index}
+                                    />
+                                );
+                            })}
+                        </Routes>
+                    </Suspense>
+                </BrowserRouter>
+            </SidebarProvider>
+        </ToastProvider>
     );
 }
 
