9493bed3bb727bb8048c1e24e21d57f3fc96baae
ng.hao.t <ng.hao.t@nghaots-iMac-Pro.local>
2025-02-08 18:20:49 +0700

    ourshopcdbn

--- src/pages/OurShop/components/Banner.jsx
+++ src/pages/OurShop/components/Banner.jsx
@@ -1,10 +1,25 @@
+import Button from '../../../components/Button/Button';
+import CountdownTimer from '../../../components/CountdownTimer/CountdownTimer';
 import styles from '../styles.module.scss';
 
 function Banner() {
-    const { containerBanner } = styles;
+    const { containerBanner, contentBox, title, boxBtn, countDownBox } = styles;
+
+    const targetDate = '2025-12-31T00:00:00';
+
     return (
         <>
-            <div className={containerBanner}></div>
+            <div className={containerBanner}>
+                <div className={contentBox}>
+                    <div className={countDownBox}>
+                        <CountdownTimer targetDate={targetDate} />
+                    </div>
+                    <div className={title}>The Classic Make A ComeBack</div>
+                    <div className={boxBtn}>
+                        <Button content='Buy Now' />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
